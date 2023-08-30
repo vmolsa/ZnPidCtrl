d0b4aaa5f4defe98a8cd780afc9bbfcb63305785
vmolsa <ville.molsa@gmail.com>
2023-08-30 23:15:47 +0300

    Remove unnecessary code.

--- src/main.zig
+++ src/main.zig
@@ -31,8 +31,6 @@ const PI = 3.14159265358979323846;
 
 pub const ZnPidCtrl = struct {
     g: ZnGains, // Default gains
-    calibrate: bool = false,
-    index: u32 = 0,
     iterations: u32 = 0, // Calibration iterations
     edge: ZnEdge = .ZnRisingEdge,
 
@@ -52,10 +50,6 @@ pub const ZnPidCtrl = struct {
     Kp: f32, // Kp (Proportional gain)
     Ki: f32, // Ti (Integral gain)
     Kd: f32, // Td (Derivative gain)
-
-    pAvarage: f32 = 0, // Avarage Kp (Proportional gain)
-    iAvarage: f32 = 0, // Avarage Ti (Integral gain)
-    dAvarage: f32 = 0, // Avarage Td (Derivative gain)
 
     ku: f32 = 0, // Ultimate gain
     tu: f32 = 0, // Period of output oscillations
@@ -95,29 +89,17 @@ pub const ZnPidCtrl = struct {
     }
 
     pub fn isCalibrating(self: *Self) bool {
-        return self.calibrate;
-    }
-
-    pub fn runCalibration(self: *Self) void {
-        self.pError = 0;
-        self.iError = 0;
-        self.dError = 0;
-
-        self.pAvarage = 0;
-        self.iAvarage = 0;
-        self.dAvarage = 0;
-
-        self.index = 0;
-        self.edge = .ZnRisingEdge;
-        self.calibrate = true;
-
-        if (self.iterations == 0) {
-            self.iterations = 1000;
-        }
+        return self.iterations > 0;
     }
 
     pub fn setIterations(self: *Self, iterations: u32) void {
         self.iterations = iterations;
+
+        self.pError = 0;
+        self.iError = 0;
+        self.dError = 0;
+
+        self.edge = .ZnRisingEdge;
     }
 
     pub fn setGains(self: *Self, gains: ZnGains) void {
@@ -134,18 +116,6 @@ pub const ZnPidCtrl = struct {
         };
     }
 
-    pub fn getAvarageGains(self: *Self) ZnGains {
-        if (self.index < 2) {
-            return self.getGains();
-        }
-
-        return ZnGains{
-            .Kp = (self.pAvarage / @as(f32, @floatFromInt(self.index - 1))),
-            .Ti = (self.iAvarage / @as(f32, @floatFromInt(self.index - 1))),
-            .Td = (self.dAvarage / @as(f32, @floatFromInt(self.index - 1))),
-        };
-    }
-
     pub fn getPid(
         self: *Self,
         timeStampUs: u64, // Timestamp in microseconds
@@ -155,12 +125,9 @@ pub const ZnPidCtrl = struct {
         maxOutput: f32, // Must be higher than minOutput value and preferably below the maximum limit value (ex: minOutput = 10% maxOutput = 50%)
         minBangBang: u32, // Min threshold value for bang-bang control (not used in during calibration) https://en.wikipedia.org/wiki/Bang%E2%80%93bang_control or min level for triggerin calibration (iterations must be greater than 0)
         maxBangBang: u32, // Max threshold value for bang-bang control (not used in during calibration) https://en.wikipedia.org/wiki/Bang%E2%80%93bang_control or max level for triggerin calibration (iterations must be greater than 0)
-        autoCalibrate: f32,
+        autoCalibrate: f32, // Threshold value for +/-(input value). iterations must be defined
+        iterations: u32, // Iterations to run auto calibrate ex: 1000
     ) f32 {
-        if (self.calibrate and self.index >= self.iterations) {
-            self.calibrate = false;
-        }
-
         if (self.iMax < input) {
             self.iMax = input;
         }
@@ -176,13 +143,13 @@ pub const ZnPidCtrl = struct {
                     self.tsHigh = timeStampUs;
                     self.diffHigh = @floatFromInt(self.tsHigh - self.tsLow);
 
-                    if (self.calibrate) {
+                    if (self.iterations > 0) {
                         self.iMax = setPoint;
                         self.ts = timeStampUs;
 
                         return minOutput;
                     }
-                } else if (self.calibrate) {
+                } else if (self.iterations > 0) {
                     self.ts = timeStampUs;
                     return maxOutput;
                 }
@@ -196,25 +163,23 @@ pub const ZnPidCtrl = struct {
                     self.ku = (4.0 * ((maxOutput - minOutput) / 2.0)) / (PI * (self.iMax - self.iMin) / 2.0);
                     self.tu = self.diffLow + self.diffHigh;
 
-                    if (self.calibrate) {
-                        if (self.index > 1) {
-                            const diff = @as(f32, @floatFromInt(timeStampUs - self.ts));
-                            const p = self.g.Kp * self.ku;
-                            const i = (p / (self.g.Ti * self.tu)) * diff;
-                            const d = (self.g.Td * p * self.tu) / diff;
+                    if (self.iterations > 0) {
+                        const diff = @as(f32, @floatFromInt(timeStampUs - self.ts));
+                        const p = self.g.Kp * self.ku;
+                        const i = (p / (self.g.Ti * self.tu)) * diff;
+                        const d = (self.g.Td * p * self.tu) / diff;
 
-                            self.pAvarage += p;
-                            self.iAvarage += i;
-                            self.dAvarage += d;
-                        }
+                        self.Kp = (self.Kp + p) / 2;
+                        self.Ki = (self.Ki + i) / 2;
+                        self.Kd = (self.Kd + d) / 2;
 
                         self.iMin = setPoint;
                         self.ts = timeStampUs;
-                        self.index += 1;
+                        self.iterations -= 1;
 
                         return maxOutput;
                     }
-                } else if (self.calibrate) {
+                } else if (self.iterations > 0) {
                     self.ts = timeStampUs;
                     return minOutput;
                 }
@@ -234,8 +199,8 @@ pub const ZnPidCtrl = struct {
             return minOutput;
         }
 
-        if (autoCalibrate > 0 and ((setPoint - input) > autoCalibrate or (input - setPoint) > autoCalibrate)) {
-            self.runCalibration();
+        if (iterations > 0 and autoCalibrate > 0 and ((setPoint - input) > autoCalibrate or (input - setPoint) > autoCalibrate)) {
+            self.setIterations(iterations);
             return maxOutput;
         }
 
@@ -272,9 +237,9 @@ fn setOutput(input: f32, speed: f32, friction: f32) f32 {
     var ret: f32 = input;
 
     if (speed >= 50) {
-        ret += (0.001337 + ((reng.random().float(f32) / 10000) * friction)) * speed;
+        ret += (0.004337 + ((reng.random().float(f32) / 10000) * friction)) * speed;
     } else {
-        ret -= ((0.001337 + ((reng.random().float(f32) / 10000) * friction)) * 5) * ((50 - speed) + 1);
+        ret -= ((0.007337 + ((reng.random().float(f32) / 10000) * friction)) * 5) * ((50 - speed) + 1);
     }
 
     return ret;
@@ -291,17 +256,15 @@ test "ZnSafe - With calibration" {
 
     std.debug.print("\nInput: {d}\n", .{input});
 
-    for (0..100000000) |us| { // Every iteration is happening in 100ms cycles
+    for (0..10000000) |us| { // Every iteration is happening in 100ms cycles
         if (!ctrl.isCalibrating()) {
-            if ((setpoint - input) > 0.99 or (input - setpoint) > 0.99) {
-                ctrl.runCalibration();
+            if ((setpoint - input) > 0.5 or (input - setpoint) > 0.5) {
+                ctrl.setIterations(1000);
                 calibrated += 1;
-            } else {
-                ctrl.setGains(ctrl.getAvarageGains());
             }
         }
 
-        speed = ctrl.getPid((us * 100000), input, setpoint, 0, 100, 0, 0, 0);
+        speed = ctrl.getPid((us * 100000), input, setpoint, 0, 100, 0, 0, 0, 0);
         input = setOutput(input, speed, 25);
 
         // if ((us % 1000) == 0) {
@@ -314,12 +277,12 @@ test "ZnSafe - With calibration" {
     }
 
     std.debug.print("Input: {d} Setpoint: {d}\n", .{ input, setpoint });
-    std.debug.print("{any}\n", .{ctrl.getAvarageGains()});
+    std.debug.print("{any}\n", .{ctrl.getGains()});
     std.debug.print("Calibrated {d} times.\n\n", .{calibrated});
 }
 
 test "Static ZnGains - Without calibration" {
-    var ctrl = ZnPidCtrl.init(.{ .Kp = 7.08424133e+02, .Ti = 3.99408843e+02, .Td = 8.47085388e+02 }, 0);
+    var ctrl = ZnPidCtrl.init(.{ .Kp = 4.86740913e+01, .Ti = 2.65768165e+01, .Td = 5.98150596e+01 }, 0);
     var input: f32 = getInput();
     var speed: f32 = 0;
     const setpoint = 668.5;
@@ -327,7 +290,7 @@ test "Static ZnGains - Without calibration" {
     std.debug.print("\nInput: {d}\n", .{input});
 
     for (0..10000000) |us| { // Every iteration is happening in 100ms cycles
-        speed = ctrl.getPid((us * 100000), input, setpoint, 0, 100, 0, 0, 0);
+        speed = ctrl.getPid((us * 100000), input, setpoint, 0, 100, 0, 0, 0, 0);
         input = setOutput(input, speed, 25);
 
         // if ((us % 1000) == 0) {
@@ -340,7 +303,7 @@ test "Static ZnGains - Without calibration" {
     }
 
     std.debug.print("Input: {d} Setpoint: {d}\n", .{ input, setpoint });
-    std.debug.print("{any}\n\n", .{ctrl.getAvarageGains()});
+    std.debug.print("{any}\n\n", .{ctrl.getGains()});
 }
 
 test "ZnSafe - With auto calibration" {
@@ -352,7 +315,7 @@ test "ZnSafe - With auto calibration" {
     std.debug.print("\nInput: {d}\n", .{input});
 
     for (0..10000000) |us| { // Every iteration is happening in 100ms cycles
-        speed = ctrl.getPid((us * 100000), input, setpoint, 0, 100, 0, 0, 0.01);
+        speed = ctrl.getPid((us * 100000), input, setpoint, 0, 100, 0, 0, 0.5, 1000);
         input = setOutput(input, speed, 25);
 
         // if ((us % 1000) == 0) {
@@ -365,5 +328,5 @@ test "ZnSafe - With auto calibration" {
     }
 
     std.debug.print("Input: {d} Setpoint: {d}\n", .{ input, setpoint });
-    std.debug.print("{any}\n\n", .{ctrl.getAvarageGains()});
+    std.debug.print("{any}\n\n", .{ctrl.getGains()});
 }
