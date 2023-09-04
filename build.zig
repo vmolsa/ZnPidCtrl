f84dd1009fa25a27258ed6bc2d84aa2bfc570a99
vmolsa <ville.molsa@gmail.com>
2023-09-04 20:51:12 +0300

    Add zig module

--- build.zig
+++ build.zig
@@ -15,6 +15,11 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    b.addModule(.{
+        .name = "ZnPidCtrl",
+        .source_file = .{ .path = "src/main.zig" },
+    });
+
     const lib = b.addStaticLibrary(.{
         .name = "ZnPidCtrl",
         // In this case the main source file is merely a path, however, in more
